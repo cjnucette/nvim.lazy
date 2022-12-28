54009894559706d2a137480460d5cd5a53110928
Carlos Nucette <cjnucette@gmail.com>
2022-12-28 19:13:15 -0400

    Move gitsigns config to plugins/init.lua

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -3,7 +3,9 @@ return {
 	'bluz71/vim-nightfly-colors',
 	'lvimuser/lsp-inlayhints.nvim',
 	'folke/neodev.nvim',
+	{ 'tpope/vim-fugitive', cmd = 'G' },
 	'b0o/schemastore.nvim',
 	{ 'catppuccin/nvim', name = 'catppuccin' },
-	{ 'windwp/nvim-autopairs', config = { check_ts = true }, event = 'BufReadPre' }
+	{ 'windwp/nvim-autopairs', config = { check_ts = true }, event = 'BufReadPre' },
+	{ 'lewis6991/gitsigns.nvim', config = true, event = 'BufReadPre' }
 }
