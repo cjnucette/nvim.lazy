1412e1880c120d95947aa8a37b9d559b5eb0dcc2
Carlos Nucette <cjnucette@gmail.com>
2022-12-28 11:01:21 -0400

    Fix: change vim.opt to vim.opt_local

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,3 +1,3 @@
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.opt.foldlevel = 99
+vim.opt_local.foldmethod = 'expr'
+vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt_local.foldlevel = 99
--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,16 +1,15 @@
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.opt.foldlevel = 99
+vim.opt_local.foldmethod = 'expr'
+vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt_local.foldlevel = 99
 
--- vim.opt.formatoptions-=cro
-vim.opt.include = 'require'
-vim.opt.define = 'function'
-vim.opt.suffixes:append('init.lua')
-vim.opt.suffixes:append('.lua')
-vim.opt.path:append('/home/cjnucette/.config/nvim/lua/user')
--- vim.opt.path:append('/home/cjnucette/.config/nvim/after/plugin')
-
-vim.cmd [[setlocal includeexpr=substitute(v:fname,'\\.','/','g')]]
+-- vim.opt_local.formatoptions-=cro
+vim.opt_local.include = 'require'
+vim.opt_local.define = 'function'
+vim.opt_local.suffixes:append('init.lua') -- not working, gf tries to open the folder.
+vim.opt_local.suffixes:append('.lua')
+vim.opt_local.path:append(vim.fn.stdpath('config') .. '/lua')
+-- vim.opt_local.includeexpr = vim.fn.substitute(vim.v.fname, '\\.', '/', 'g')
+vim.cmd [[setlocal includeexpr=substitute(v:fname,'\\.','/','g')]] -- so it resolves 'user.somefile' as 'user/somefile'
 
 require('user.utils').map('n', '<leader>cl', 'viwyoP(<c-r>*)<esc>', { buffer = 0 })
 require('user.utils').map('n', '<leader>cli', 'viwyoprint(vim.inspect(<c-r>*))<esc>', { buffer = 0 })
