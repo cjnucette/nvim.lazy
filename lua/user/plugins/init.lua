31a4f4fc0346508f67517721a7d8d8593c94d77a
Carlos Nucette <cjnucette@gmail.com>
2022-12-28 19:25:43 -0400

    Move surround conf to plugins/init.lua

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -3,10 +3,12 @@ return {
 	'bluz71/vim-nightfly-colors',
 	'lvimuser/lsp-inlayhints.nvim',
 	'folke/neodev.nvim',
-	{ 'tpope/vim-fugitive', cmd = 'G' },
 	'b0o/schemastore.nvim',
+	{ 'tpope/vim-fugitive', cmd = 'G' },
 	{ 'catppuccin/nvim', name = 'catppuccin' },
 	{ 'windwp/nvim-autopairs', config = { check_ts = true }, event = 'BufReadPre' },
 	{ 'lewis6991/gitsigns.nvim', config = true, event = 'BufReadPre' },
-	{ 'ethanholz/nvim-lastplace', config = true, event = 'BufReadPre' }
+	{ 'ethanholz/nvim-lastplace', config = true, event = 'BufReadPre' },
+	{'kylechui/nvim-surround', version = "*", config = true, event = 'InsertEnter'}
 }
+
