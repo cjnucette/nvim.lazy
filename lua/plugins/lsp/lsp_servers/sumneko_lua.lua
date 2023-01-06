792e45038fb6dcdf939386172cb39af8d1705e6e
Carlos Nucette <cjnucette@gmail.com>
2023-01-05 20:35:27 -0400

    Remove unnecessary settings

--- lua/plugins/lsp/lsp_servers/sumneko_lua.lua
+++ lua/plugins/lsp/lsp_servers/sumneko_lua.lua
@@ -1,20 +1,5 @@
 local opts = {}
 
---[[
-local package_prefix = vim.fn.expand((vim.env.XDG_CONFIG_HOME or '~/.config/nvim'))
-local function is_package_path(path)
-	if package_prefix ~= string.sub(path, 1, #package_prefix) then
-		return false
-	end
-
-	return vim.fn.isdirectory(path .. '/lua') ~= 0
-end
-]]
-
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
-
 opts.on_attach = function(client, _)
 	client.server_capabilities.documentFormattingProvider = true
 	client.server_capabilities.documentRangeFormattingProvider = true
@@ -24,7 +9,6 @@ opts.settings = {
 	Lua = {
 		runtime = {
 			version = 'LuaJIT',
-			path = runtime_path,
 		},
 		format = {
 			enable = true,
@@ -49,14 +33,10 @@ opts.settings = {
 		},
 		completion = {
 			keywordSnippet = 'Replace',
-			-- callSnippet = 'Replace'
 			callSnippet = 'Replace',
 			workspaceWord = true,
 		},
 		workspace = {
-			-- library = library,
-			-- library = vim.tbl_filter(is_package_path, vim.api.nvim_get_runtime_file('', true)),
-			library = vim.api.nvim_get_runtime_file('', true),
 			checkThirdParty = false,
 		},
 	},
