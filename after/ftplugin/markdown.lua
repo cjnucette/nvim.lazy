3f129eb871e34ed7e18814b3f8c98366cdafa921
Carlos Nucette <cjnucette@gmail.com>
2023-01-11 13:42:57 -0400

    Remove mappings

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,10 +1,4 @@
-local map = require('utils').map
-local opts = { buffer = 0 }
-
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
 vim.opt_local.breakindent = true
 vim.opt_local.conceallevel = 2
-
-map('n', '<tab>', "<cmd>call search('\\[[^]]*\\]([^)]\\+)')<cr>", opts)
-map('n', '<s-tab>', "<cmd>call search('\\[[^]]*\\]([^)]\\+)', 'b')<cr>", opts)
