975414ef0ad7b173f7f20f4cfa554d93c7b1d7f7
Carlos Nucette <cjnucette@gmail.com>
2023-01-15 05:02:20 -0400

    Change gitsign conf

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,14 @@
 return {
-	{ 'folke/tokyonight.nvim', lazy = false, priority = 1000 },
 	'bluz71/vim-nightfly-colors',
 	'b0o/schemastore.nvim',
+	{ 'folke/tokyonight.nvim', lazy = false, priority = 1000 },
 	{ 'rafcamlet/nvim-luapad', cmd = { 'Luapad', 'LuaRun' } },
 	{ 'tpope/vim-fugitive', cmd = 'G' },
 	{ 'catppuccin/nvim', name = 'catppuccin' },
 	{ 'windwp/nvim-autopairs', opts = { check_ts = true }, event = 'BufReadPre' },
-	{ 'lewis6991/gitsigns.nvim', config = true, event = 'BufReadPre' },
 	-- { 'ethanholz/nvim-lastplace', config = true, event = 'BufReadPre' },
 	{ 'kylechui/nvim-surround', version = '*', config = true, event = 'InsertEnter' },
+	{ 'stevearc/oil.nvim', config = true, cmd = 'Oil' },
 	{ 'lvimuser/lsp-inlayhints.nvim', opts = {
 		inlay_hints = {
 			type_hints = {
@@ -17,7 +17,12 @@ return {
 			only_current_line = true
 		}
 	} },
-	{ 'stevearc/oil.nvim', config = true, cmd = 'Oil' },
+	{ 'lewis6991/gitsigns.nvim', opts = {
+		numhl = true,
+		signcolumn = false
+	},
+		event = 'BufReadPre'
+	},
 	-- dev
-	-- { 'cjnucette/scripts.nvim', cmd = 'Scripts', dev = true }
+	{ 'cjnucette/scripts.nvim', cmd = 'Scripts', dev = true }
 }
