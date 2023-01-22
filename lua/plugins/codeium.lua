cb60c6c0295f8ab344f690c51ae7a01a3ff10368
Carlos Nucette <cjnucette@gmail.com>
2023-01-22 14:34:56 -0400

    Add codeium plugin

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -2,11 +2,11 @@ local map = require('utils').map
 
 return {
 	'Exafunction/codeium.vim',
-	event = "VeryLazy",
+	event = 'VeryLazy',
 	config = function()
-		map('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, {expr = true, desc = "Codeium: Next completion"} )
-		map('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, {expr = true, desc = "Codeium: Prev completion"} )
-		map('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, {expr = true, desc = "Codeium: Clear suggestions"} )
-		map('i', '<C-cr>', function() return vim.fn['codeium#Accept']() end, {expr = true, desc = "Codeium: Accepts suggestion"} )
+		map('i', '<C-p>', '<Plug>(codeium-next)', { desc = 'Codeium: Next completion' })
+		map('i', '<C-n>', '<Plug>(codeium-previous)', { desc = 'Codeium: Prev completion' })
+		map('i', '<C-c>', '<Plug>(codeium-dismiss)', { desc = 'Codeium: Clear suggestions' })
+		map('i', '<C-cr>', '<Plug>(codeium-complete)', { desc = 'Codeium: Accepts suggestion' })
 	end
 }
