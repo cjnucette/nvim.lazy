b6cc530c27ac38bc358c6fec67401b3d1dc357d7
Carlos Nucette <cjnucette@gmail.com>
2023-01-23 17:27:16 -0400

    Disable auto insert comments

--- after/ftplugin/javascript.lua
+++ after/ftplugin/javascript.lua
@@ -1,6 +1,7 @@
 vim.opt_local.foldmethod = 'expr'
 vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.opt_local.foldlevel = 99
+vim.cmd [[setlocal formatoptions-=cro]]
 
 -- nnoremap <buffer><silent> <leader>cl viwyoconsole.log({<c-r>*});<esc>
 require('utils').map('n', '<leader>cl', 'viwyoconsole.log({<C-r>*});<esc>', { buffer = 0 })
