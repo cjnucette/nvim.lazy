43043459b1c3fc4c6fa3655aaf385de2907c2a30
Carlos Nucette <cjnucette@gmail.com>
2023-01-28 08:31:47 -0400

    Fix it attaches on deno projects

--- lua/plugins/lsp/lsp_servers/tsserver.lua
+++ lua/plugins/lsp/lsp_servers/tsserver.lua
@@ -3,24 +3,26 @@ local opts = {}
 local util = require('lspconfig.util')
 
 local inlay_hints = {
-  includeInlayEnumMemberValueHints = true,
-  includeInlayFunctionLikeReturnTypeHints = true,
-  includeInlayFunctionParameterTypeHints = true,
-  includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-  includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-  includeInlayPropertyDeclarationTypeHints = true,
-  includeInlayVariableTypeHints = true,
+	includeInlayEnumMemberValueHints = true,
+	includeInlayFunctionLikeReturnTypeHints = true,
+	includeInlayFunctionParameterTypeHints = true,
+	includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+	includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+	includeInlayPropertyDeclarationTypeHints = true,
+	includeInlayVariableTypeHints = true,
 }
 
 opts.root_dir = util.root_pattern('package.json', 'tsconfig.json', 'jsconfig.json')
 
 opts.settings = {
-  javascript = {
-    inlayHints = inlay_hints,
-  },
-  typescript = {
-    inlayHints = inlay_hints,
-  }
+	javascript = {
+		inlayHints = inlay_hints,
+		single_file_support = false
+	},
+	typescript = {
+		inlayHints = inlay_hints,
+		single_file_support = false
+	}
 }
 
 return opts
