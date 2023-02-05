df830c3ee48e56989785f8db330fc6973ae48ce2
Carlos Nucette <cjnucette@gmail.com>
2023-02-05 16:35:00 -0400

    Replace vsnip with luasnip, refactor

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,14 +1,9 @@
-local map = require('utils').map
-
 return {
-	'Exafunction/codeium.vim',
-	lazy = false,
-	enabled = false,
-	config = function()
-		-- vim.g.codeium_disable_bindings = true
-		-- map('i', '<C-p>', '<Plug>(codeium-next)', { desc = 'Codeium: Next completion' })
-		-- map('i', '<C-n>', '<Plug>(codeium-previous)', { desc = 'Codeium: Prev completion' })
-		-- map('i', '<C-c>', '<Plug>(codeium-dismiss)', { desc = 'Codeium: Clear suggestions' })
-		-- map('i', '<C-cr>', '<Plug>(codeium-complete)', { desc = 'Codeium: Accepts suggestion' })
-	end
+	'jcdickinson/codeium.nvim',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'MunifTanjim/nui.nvim'
+	},
+	config = true
 }
+
