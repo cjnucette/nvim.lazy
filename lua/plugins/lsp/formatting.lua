f23504b65f0d5ba7d2560dce70739c8884c4eee4
Carlos Nucette <cjnucette@gmail.com>
2023-02-14 09:45:18 -0400

    Fix typo in mapping description, reformat

--- lua/plugins/lsp/formatting.lua
+++ lua/plugins/lsp/formatting.lua
@@ -10,22 +10,27 @@ function M.setup(client, bufnr)
 	end
 
 	if client.supports_method('textDocument/formatting') then
-		vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-			if vim.lsp.buf.format then
-				vim.lsp.buf.format()
-			elseif vim.lsp.buf.formatting then
-				vim.lsp.buf.formatting()
-			end
-			end, { desc = 'Format current buffer with LSP' })
+		vim.api.nvim_buf_create_user_command(
+			bufnr,
+			'Format',
+			function(_)
+				if vim.lsp.buf.format then
+					vim.lsp.buf.format()
+				elseif vim.lsp.buf.formatting then
+					vim.lsp.buf.formatting()
+				end
+			end,
+			{ desc = 'Format current buffer with LSP' }
+		)
 
 		-- maps
 		nmap('<leader>f', function()
 			vim.cmd.Format()
-			end, '[F]ormat and save current buffer')
+		end, '[F]ormat current buffer')
 		nmap('<leader>fs', function()
 			vim.cmd.Format()
 			vim.cmd.w()
-			end, '[F]ormat and [s]ave current buffer')
+		end, '[F]ormat and [s]ave current buffer')
 	end
 end
 
