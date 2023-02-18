f4eef69ca0ba5f962881a32682d60e475b222a2d
Carlos Nucette <cjnucette@gmail.com>
2023-02-18 08:03:11 -0400

    Manage when to load autocmd and mappings

--- init.lua
+++ init.lua
@@ -1,18 +1,28 @@
 -- load options here, so options are correctly applied
 require('config.options')
--- Some autocmds don't work if loaded in the autocmd below
-require('config.autocmds')
 
 -- 💤  plugin manager
 require('config.lazy')
 
+-- Some autocmds don't work if loaded in the autocmd below
 -- mappings and autocmds can wait
-vim.api.nvim_create_autocmd('User', {
-	pattern = 'VeryLazy',
-	callback = function()
-		require('config.mappings')
-	end
-})
+if vim.fn.argc( -1) == 0 then
+	vim.api.nvim_create_autocmd('User', {
+		desc = 'autocmds and mappings can wait to load',
+		group = vim.api.nvim_create_augroup('lazyvim', { clear = true }),
+		pattern = 'VeryLazy',
+		callback = function()
+			require('config.autocmds')
+			require('config.mappings')
+		end
+	})
+else
+	require('config.autocmds')
+	require('config.mappings')
+end
 
 -- get bash aliases
 vim.env.BASH_ENV = './.vim_bash_env'
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
