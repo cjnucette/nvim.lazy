00e9070c12bc6cbedc411a494baea3a5ea41cabe
Carlos Nucette <cjnucette@gmail.com>
2023-03-03 05:32:52 -0400

    Add updated ts-rainbow plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,7 +10,8 @@ return {
 		'nvim-treesitter/nvim-treesitter-textobjects',
 		'windwp/nvim-ts-autotag',
 		'JoosepAlviste/nvim-ts-context-commentstring',
-		'kevinhwang91/nvim-ufo'
+		'kevinhwang91/nvim-ufo',
+		'HiPhish/nvim-ts-rainbow2'
 	},
 	keys = {
 		{ '<C-space>', desc = 'Treesitter: Increment selection' },
@@ -57,44 +58,48 @@ return {
 					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 					keymaps = {
 						-- You can use the capture groups defined in textobjects.scm
-						['aa'] = '@parameter.outer',
-						['ia'] = '@parameter.inner',
-						['af'] = '@function.outer',
-						['if'] = '@function.inner',
-						['ac'] = '@class.outer',
-						['ic'] = '@class.inner',
+							['aa'] = '@parameter.outer',
+							['ia'] = '@parameter.inner',
+							['af'] = '@function.outer',
+							['if'] = '@function.inner',
+							['ac'] = '@class.outer',
+							['ic'] = '@class.inner',
 					},
 				},
 				move = {
 					enable = true,
 					set_jumps = true, -- whether to set jumps in the jumplist
 					goto_next_start = {
-						[']m'] = '@function.outer',
-						[']]'] = '@class.outer',
+							[']m'] = '@function.outer',
+							[']]'] = '@class.outer',
 					},
 					goto_next_end = {
-						[']M'] = '@function.outer',
-						[']['] = '@class.outer',
+							[']M'] = '@function.outer',
+							[']['] = '@class.outer',
 					},
 					goto_previous_start = {
-						['[m'] = '@function.outer',
-						['[['] = '@class.outer',
+							['[m'] = '@function.outer',
+							['[['] = '@class.outer',
 					},
 					goto_previous_end = {
-						['[M'] = '@function.outer',
-						['[]'] = '@class.outer',
+							['[M'] = '@function.outer',
+							['[]'] = '@class.outer',
 					},
 				},
 				swap = {
 					enable = true,
 					swap_next = {
-						['<leader>a'] = '@parameter.inner',
+							['<leader>a'] = '@parameter.inner',
 					},
 					swap_previous = {
-						['<leader>A'] = '@parameter.inner',
+							['<leader>A'] = '@parameter.inner',
 					},
 				},
 			},
+			rainbow = {
+				enable = true,
+				strategy = require('ts-rainbow.strategy.local')
+			}
 		}
 	end
 }
