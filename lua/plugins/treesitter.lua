c160c7269cee60a98399a5cb29ebd22777f876cd
Carlos Nucette <cjnucette@gmail.com>
2023-04-08 06:01:39 -0400

    Rename treesitter help parser to vimdoc

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,8 +15,8 @@ return {
 	},
 	keys = {
 		{ '<C-space>', desc = 'Treesitter: Increment selection' },
-		{ '<bs>',      desc = 'Treesitter: Decrement selection',         mode = 'x' },
-		{ '<C-s>',     desc = 'Treesitter: Increment selection by scope' },
+		{ '<bs>', desc = 'Treesitter: Decrement selection', mode = 'x' },
+		{ '<C-s>', desc = 'Treesitter: Increment selection by scope' },
 	},
 	config = function()
 		require('nvim-treesitter.configs').setup {
@@ -30,7 +30,7 @@ return {
 				'javascript',
 				'typescript',
 				'tsx',
-				'help',
+				'vimdoc',
 				'bash',
 				'css',
 				'html',
@@ -58,41 +58,41 @@ return {
 					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 					keymaps = {
 						-- You can use the capture groups defined in textobjects.scm
-							['aa'] = '@parameter.outer',
-							['ia'] = '@parameter.inner',
-							['af'] = '@function.outer',
-							['if'] = '@function.inner',
-							['ac'] = '@class.outer',
-							['ic'] = '@class.inner',
+						['aa'] = '@parameter.outer',
+						['ia'] = '@parameter.inner',
+						['af'] = '@function.outer',
+						['if'] = '@function.inner',
+						['ac'] = '@class.outer',
+						['ic'] = '@class.inner',
 					},
 				},
 				move = {
 					enable = true,
 					set_jumps = true, -- whether to set jumps in the jumplist
 					goto_next_start = {
-							[']m'] = '@function.outer',
-							[']]'] = '@class.outer',
+						[']m'] = '@function.outer',
+						[']]'] = '@class.outer',
 					},
 					goto_next_end = {
-							[']M'] = '@function.outer',
-							[']['] = '@class.outer',
+						[']M'] = '@function.outer',
+						[']['] = '@class.outer',
 					},
 					goto_previous_start = {
-							['[m'] = '@function.outer',
-							['[['] = '@class.outer',
+						['[m'] = '@function.outer',
+						['[['] = '@class.outer',
 					},
 					goto_previous_end = {
-							['[M'] = '@function.outer',
-							['[]'] = '@class.outer',
+						['[M'] = '@function.outer',
+						['[]'] = '@class.outer',
 					},
 				},
 				swap = {
 					enable = true,
 					swap_next = {
-							['<leader>a'] = '@parameter.inner',
+						['<leader>a'] = '@parameter.inner',
 					},
 					swap_previous = {
-							['<leader>A'] = '@parameter.inner',
+						['<leader>A'] = '@parameter.inner',
 					},
 				},
 			},
