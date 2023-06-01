8a041e8683a6bfb6c2051d59cbce3bc33a02ad49
Carlos Nucette <cjnucette@gmail.com>
2023-06-01 05:51:53 -0400

    Add map to read current line

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -21,8 +21,12 @@ map('v', '>', '>gv', { desc = 'Indent current selection and keep the selection' 
 map('n', '<a-j>', ':m .+1<CR>==', { desc = 'Move current line down keeping indentation' })
 map('n', '<a-k>', ':m .-2<CR>==', { desc = 'Move current line up keeping indentation' })
 map('v', '<a-j>', ":m '>+1<CR>gv=gv",
-    { desc = 'Move current line up, keeping indentation and selection' })
+	{ desc = 'Move current line up, keeping indentation and selection' })
 map('v', '<a-k>', ":m '<-2<CR>gv=gv",
-    { desc = 'Move current line up, keeping indentation and selection' })
+	{ desc = 'Move current line up, keeping indentation and selection' })
 
 map('n', '<leader>cn', ':help news<CR>', { desc = '[C]heck what is [n]ew in neovim' })
+
+-- text to speech
+map('n', '<leader>r', [[:execute 'silent !google_speech ' . '"' . getline('.') . '"'<cr>]],
+	{ desc = '[R]ead aloud the current line' })
