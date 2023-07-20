381fb0a944911b1a90d5bcafa9ee03d869ca07a9
Carlos Nucette <cjnucette@gmail.com>
2023-07-20 08:32:25 -0400

    Add guard plugins to replace null-ls

---
+++ lua/plugins/guard.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		'nvimdev/guard.nvim',
+		event = { 'BufReadPre', 'BufNewFile' },
+		init = function()
+			local gft = require('guard.filetype')
+
+			local shfmt = {
+				cmd = 'shfmt',
+				args = { '-i', '4', '-ci' },
+				stdin = true,
+			}
+
+			gft('html'):fmt('prettier')
+			gft('css'):fmt('prettier')
+			gft('javascript'):fmt('prettier')
+			gft('javascriptreact'):fmt('prettier')
+			gft('typescript'):fmt('prettier')
+			gft('typescriptreact'):fmt('prettier')
+			gft('markdown'):fmt('prettier')
+			gft('json'):fmt('prettier')
+			gft('bash'):fmt(shfmt)
+			gft('sh'):fmt(shfmt)
+		end,
+		opts = {
+			format_on_save = true
+		}
+	},
+}
