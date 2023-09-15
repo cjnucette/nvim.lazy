150c68aac5c6353db2702cc7b38801926efbdc66
Carlos Nucette <cjnucette@gmail.com>
2023-09-15 14:15:08 -0400

    Disable guard plugin

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -2,6 +2,7 @@ return {
 	{
 		'nvimdev/guard.nvim',
 		event = { 'BufReadPre', 'BufNewFile' },
+		enabled = false,
 		init = function()
 			local gft = require('guard.filetype')
 
@@ -10,15 +11,35 @@ return {
 				args = { '-i', '4', '-ci' },
 				stdin = true,
 			}
+			local prettier = {
+				cmd = 'prettier',
+				args = {
+					'--config-precedence',
+					'prefer-file',
+					'--single-quote',
+					'true',
+					'--bracket-same-line',
+					'false',
+					'--trailing-comma',
+					'es5',
+					'--tab-width',
+					'4',
+					'--use-tabs',
+					'true',
+					'--print-width',
+					'120',
+				},
+				stdin = true,
+			}
 
-			gft('html'):fmt('prettier')
-			gft('css'):fmt('prettier')
-			gft('javascript'):fmt('prettier')
-			gft('javascriptreact'):fmt('prettier')
-			gft('typescript'):fmt('prettier')
-			gft('typescriptreact'):fmt('prettier')
-			gft('markdown'):fmt('prettier')
-			gft('json'):fmt('prettier')
+			gft('html'):fmt(prettier)
+			gft('css'):fmt(prettier)
+			gft('javascript'):fmt(prettier)
+			gft('javascriptreact'):fmt(prettier)
+			gft('typescript'):fmt(prettier)
+			gft('typescriptreact'):fmt(prettier)
+			gft('markdown'):fmt(prettier)
+			gft('json'):fmt(prettier)
 			gft('bash'):fmt(shfmt)
 			gft('sh'):fmt(shfmt)
 		end,
