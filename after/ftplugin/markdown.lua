7897baffc589074f74fd75494737453051902619
Carlos Nucette <cjnucette@gmail.com>
2023-11-11 07:21:55 -0400

    Add shortcut to open word under cursor as e new file

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,3 +2,5 @@ vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
 vim.opt_local.breakindent = true
 vim.opt_local.conceallevel = 2
+
+vim.keymap.set('n', '<leader>cm', "':e <c-r><c-w>.md'", { expr = true, desc = 'Markdown: [c]reates file under cursor' })
