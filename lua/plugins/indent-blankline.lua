7d00eba0dc78b4190c67ef8cd3740de7f21de8a5
Carlos Nucette <cjnucette@gmail.com>
2023-11-19 12:41:46 -0400

    Disable ibl

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,8 +2,9 @@ return {
 	'lukas-reineke/indent-blankline.nvim',
 	event = { 'BufRead', 'BufNewFile' },
 	main = 'ibl',
+	enabled = false,
 	opts = {
-		enabled = true,
+		-- enabled = true,
 		exclude = {
 			filetypes = { 'neo-tree', 'lazy', 'mason' },
 		},
