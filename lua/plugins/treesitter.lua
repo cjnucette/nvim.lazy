c38ba1720f2c6f04c0da7219fff839c263687ead
Carlos Nucette <cjnucette@gmail.com>
2023-12-11 08:46:51 -0400

    Change ts-context-commentstring to new conf

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,32 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	version = false,
-	build = ':TSUpdate',
-	event = { 'BufReadPost', 'BufNewFile' },
-	dependencies = {
-		'windwp/nvim-ts-autotag',
+	{
+		'nvim-treesitter/nvim-treesitter',
+		version = false,
+		build = ':TSUpdate',
+		event = { 'BufReadPost', 'BufNewFile' },
+		dependencies = {
+			'windwp/nvim-ts-autotag',
+		},
+		keys = {
+			{ '<C-space>', desc = 'Treesitter: Increment selection' },
+			{ '<bs>', desc = 'Treesitter: Decrement selection', mode = 'x' },
+			{ '<C-s>', desc = 'Treesitter: Increment selection by scope' },
+		},
+		config = function()
+			require('nvim-treesitter.configs').setup {
+				highlight = { enable = true },
+				indent = { enable = true },
+				sync_install = false,
+				modules = {},
+				ignore_install = {},
+				auto_install = true,
+				ensure_installed = { 'lua', 'vim' },
+				autotag = { enable = true },
+			}
+		end
+	},
+	{
 		'JoosepAlviste/nvim-ts-context-commentstring',
-	},
-	keys = {
-		{ '<C-space>', desc = 'Treesitter: Increment selection' },
-		{ '<bs>', desc = 'Treesitter: Decrement selection', mode = 'x' },
-		{ '<C-s>', desc = 'Treesitter: Increment selection by scope' },
-	},
-	config = function()
-		require('nvim-treesitter.configs').setup {
-			highlight = { enable = true },
-			indent = { enable = true },
-			sync_install = false,
-			modules = {},
-			ignore_install = {},
-			auto_install = true,
-			ensure_installed = {'lua', 'vim'},
-			autotag = { enable = true },
-			context_commentstring = { enable = true },
-		}
-	end
+		config = true,
+	}
 }
