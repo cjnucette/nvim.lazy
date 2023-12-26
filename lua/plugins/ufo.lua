53cd26cb0de62f96599661bf5a3c6dc891482584
Carlos Nucette <cjnucette@gmail.com>
2023-12-26 12:41:26 -0400

    Change columns configuration

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -57,17 +57,19 @@ return {
 	},
 	{
 		'luukvbaal/statuscol.nvim',
-		enabled = false, -- check later nvim bug?
-		opts = function()
+		-- enabled = false, -- check later nvim bug?
+		config = function()
 			local builtin = require('statuscol.builtin')
-			return {
+			require('statuscol').setup({
 				relculright = true,
 				segments = {
 					{ text = { ' ', builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
-					{ text = { '%s' }, click = 'v:lua.ScSa' },
+					{ sign = { name = { 'Diagnostic' }, maxwidth = 2, auto = true }, click = 'v:lua.ScSa' },
+					-- { text = { '%s' }, click = 'v:lua.ScSa' },
 					{ text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
+					{ sign = { name = { '.*' }, maxwidth = 2, colwidth = 1, auto = true }, click = 'v:lua.ScSa' },
 				}
-			}
+			})
 		end
 	}
 }
